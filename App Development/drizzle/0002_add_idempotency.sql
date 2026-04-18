be4df3ddde21f59b38e4091fa514f5071197405e
Topher <Christopherwonder@gmail.com>
2026-04-18 04:26:55 -0500

    Modified by www.SourceFiles.app

--- App Development/drizzle/0002_add_idempotency.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "transactions"
-  ADD COLUMN "idempotency_key" text;
-
-CREATE UNIQUE INDEX "idx_transactions_idempotent"
-  ON "transactions" ("transaction_type", "idempotency_key")
-  WHERE idempotency_key IS NOT NULL;
